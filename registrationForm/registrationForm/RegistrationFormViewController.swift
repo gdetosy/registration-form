32cf8f5e5cc419a9dc28e8d3f248607661be363c
gdetosy <tosyjdm@gmail.com>
2022-11-20 20:11:21 +0300

    outletsRegistrForm

---
+++ registrationForm/registrationForm/RegistrationFormViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RegistrationFormViewController.swift
+//  registrationForm
+//
+//  Created by tosy on 20.11.22.
+//
+
+import UIKit
+
+class RegistrationFormViewController: UIViewController {
+    @IBOutlet var textFieldMail: UITextField!
+
+    @IBOutlet var errorMail: UILabel!
+    
+    @IBOutlet var textFieldPassword: UITextField!
+    
+    @IBOutlet var errorPasword: UILabel!
+    
+    @IBOutlet var colorIndicatorPassword: [UIView]!
+    
+    @IBOutlet var textFieldConfirmPassword: UITextField!
+    
+    @IBOutlet var errorConfirm: UILabel!
+    @IBOutlet var creatAcountButton: UIButton!
+  
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+    }
+    */
+}
