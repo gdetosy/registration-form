50c3294955d605ffa778083ce697963bc1102f0b
gdetosy <tosyjdm@gmail.com>
2022-11-24 22:07:34 +0300

    label

---
+++ registrationForm/registrationForm/SecretViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecretViewController.swift
+//  registrationForm
+//
+//  Created by tosy on 24.11.22.
+//
+
+import UIKit
+
+class SecretViewController: UIViewController{
+    
+    @IBOutlet weak var entryLbl: UILabel!
+    
+
+    @IBOutlet weak var secretCodeLabel: UILabel!
+    
+    @IBOutlet weak var secretCodeTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
