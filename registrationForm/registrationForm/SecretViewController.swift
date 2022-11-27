3b762b83b271eb0e9bf8bf00bbc11838c08efc7b
gdetosy <tosyjdm@gmail.com>
2022-11-27 13:47:52 +0300

    timer

--- registrationForm/registrationForm/SecretViewController.swift
+++ registrationForm/registrationForm/SecretViewController.swift
@@ -10,28 +10,45 @@ import UIKit
 class SecretViewController: UIViewController {
     @IBOutlet var headText: UILabel!
     @IBOutlet var secretCodeText: UILabel!
+    @IBOutlet weak var errorSecretCodeLbl: UILabel!
     @IBOutlet var secretCodeTextField: UITextField!
     var userModel: User?
     let secretCode = Int.random(in: 100000...999999)
-    
+    var sleepTime = 3
     override func viewDidLoad() {
         super.viewDidLoad()
        userCode()
       helloNameLbl()  // Do any additional setup after loading the view.
     }
     
+    @IBAction func secretCodeTextField(_ sender: UITextField) {
+        guard let text = sender.text,
+              !text.isEmpty, text == secretCode.description else {
+            errorSecretCodeLbl.text = "Error code. Plese wait \(sleepTime) seconds"
+                  sender.isUserInteractionEnabled = false
+            errorSecretCodeLbl.isHidden = false
+                  let dispatchAfter = DispatchTimeInterval.seconds(sleepTime)
+                  let deadline = DispatchTime.now() + dispatchAfter
+                  DispatchQueue.main.asyncAfter(deadline: deadline) {
+                      sender.isUserInteractionEnabled = true
+                      self.errorSecretCodeLbl.isHidden = true
+                      self.sleepTime *= 2
+                  }
+                  return
+              }
+//              performSegue(withIdentifier: "goToWelcomeVC", sender: nil)
+          }
+        
+        
+        
+        
+        
+        
     
     
-    // MARK: - Navigation
     
+    // MARK: -
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let destVC = segue.destination as? WelcomeUser
-//
-//        else {return}
-//        destVC.userModel = userModel
-//    }
-   
     func helloNameLbl() {
         guard (userModel?.name?.count)! > 0 else { headText.text = "Hello user, please enter secret code"
             return
