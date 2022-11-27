91f9bee4d7219ef82b87435b0b3810b80b6b8ab2
gdetosy <tosyjdm@gmail.com>
2022-11-27 16:16:35 +0300

    welcome VC

--- registrationForm/registrationForm/SecretViewController.swift
+++ registrationForm/registrationForm/SecretViewController.swift
@@ -10,42 +10,36 @@ import UIKit
 class SecretViewController: UIViewController {
     @IBOutlet var headText: UILabel!
     @IBOutlet var secretCodeText: UILabel!
-    @IBOutlet weak var errorSecretCodeLbl: UILabel!
+    @IBOutlet var errorSecretCodeLbl: UILabel!
     @IBOutlet var secretCodeTextField: UITextField!
     var userModel: User?
-    let secretCode = Int.random(in: 100000...999999)
+    let secretCode = Int.random(in: 100000 ... 999999)
     var sleepTime = 3
     override func viewDidLoad() {
         super.viewDidLoad()
-       userCode()
-      helloNameLbl()  // Do any additional setup after loading the view.
+        userCode()
+        helloNameLbl() // Do any additional setup after loading the view.
+        hideClaviature()
     }
     
     @IBAction func secretCodeTextField(_ sender: UITextField) {
         guard let text = sender.text,
-              !text.isEmpty, text == secretCode.description else {
+              !text.isEmpty, text == secretCode.description
+        else {
             errorSecretCodeLbl.text = "Error code. Plese wait \(sleepTime) seconds"
-                  sender.isUserInteractionEnabled = false
+            sender.isUserInteractionEnabled = false
             errorSecretCodeLbl.isHidden = false
-                  let dispatchAfter = DispatchTimeInterval.seconds(sleepTime)
-                  let deadline = DispatchTime.now() + dispatchAfter
-                  DispatchQueue.main.asyncAfter(deadline: deadline) {
-                      sender.isUserInteractionEnabled = true
-                      self.errorSecretCodeLbl.isHidden = true
-                      self.sleepTime *= 2
-                  }
-                  return
-              }
-//              performSegue(withIdentifier: "goToWelcomeVC", sender: nil)
-          }
-        
-        
-        
-        
-        
-        
-    
-    
+            let dispatchAfter = DispatchTimeInterval.seconds(sleepTime)
+            let deadline = DispatchTime.now() + dispatchAfter
+            DispatchQueue.main.asyncAfter(deadline: deadline) {
+                sender.isUserInteractionEnabled = true
+                self.errorSecretCodeLbl.isHidden = true
+                self.sleepTime *= 2
+            }
+            return
+        }
+                      performSegue(withIdentifier: "WelcomeViewController", sender: nil)
+    }
     
     // MARK: -
     
@@ -55,9 +49,8 @@ class SecretViewController: UIViewController {
         }
         headText.text = "Hello \(userModel?.name ?? ""), please enter secret code"
     }
-
+    
     func userCode() {
         secretCodeText.text = " secret code \(secretCode) for \(userModel?.email ?? " ")"
     }
-    
 }
